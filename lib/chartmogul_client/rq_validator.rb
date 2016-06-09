a0bcf201e8f29ff6db05da9178b7c5ecfe9f3cb3
Damian Jaszczurowski <damian.jaszczurowski@gmail.com>
2016-06-09 20:21:41 +0200

    Allow to create custom validators for every request

--- lib/chartmogul_client/rq_validator.rb
+++ lib/chartmogul_client/rq_validator.rb
@@ -1,5 +1,30 @@
 module ChartmogulClient::RqValidator
   def self.valid?(request)
-    raise "NIY"
+    validator = try_find_custom_validator(request)
+
+    if validator
+      validator.valid?(request)
+    else
+      [true, []]
+    end
   end
+
+  def self.try_find_custom_validator(request)
+    splitted_namespaces = request.class.name.split('::')
+    validator_scope = splitted_namespaces[0..-2].join('::')
+    validator_name =
+      "#{validator_scope}::Validators::#{splitted_namespaces[-1]}Validator"
+
+    if Object.const_defined?(validator_name)
+      validator = Object.const_get(validator_name)
+
+      unless validator.respond_to?(:valid?)
+        raise "Validator #{validator_name} should respond to valid?"
+      end
+
+      validator
+    end
+  end
+
+  private_class_method :try_find_custom_validator
 end
---
+++ spec/rq_validator_spec.rb
@@ -0,0 +1,53 @@
+require 'spec_helper'
+
+describe ChartmogulClient::RqValidator do
+  module ChartmogulClient::V1::Test
+    module Validators
+      module NoValidMethodRqValidator
+      end
+
+      module WithValidatorRqValidator
+        def self.valid?(rq)
+          raise "NIY"
+        end
+      end
+    end
+
+    class NoValidatorRq < ChartmogulClient::V1::BaseRq
+    end
+
+    class NoValidMethodRq < ChartmogulClient::V1::BaseRq
+    end
+
+    class WithValidatorRq < ChartmogulClient::V1::BaseRq
+    end
+  end
+
+  subject { described_class }
+
+  context 'when no validator for request found' do
+    it 'returns true' do
+      rq = ChartmogulClient::V1::Test::NoValidatorRq.new
+
+      expect(subject.valid?(rq)).to eq([true, []])
+    end
+  end
+
+  context 'when validator found but valid? method doesnt exist' do
+    it 'raises an error' do
+      rq = ChartmogulClient::V1::Test::NoValidMethodRq.new
+
+      expect { subject.valid?(rq) }.to raise_error("Validator ChartmogulClient::V1::Test::Validators::NoValidMethodRqValidator should respond to valid?")
+    end
+  end
+
+  context 'when validator for request found in tested_rq_directory/validators/tested_rq_validator' do
+    it 'returns validator result' do
+      rq = ChartmogulClient::V1::Test::WithValidatorRq.new
+      validator_result = [true, []]
+
+      expect(ChartmogulClient::V1::Test::Validators::WithValidatorRqValidator).to receive(:valid?).with(rq).and_return(validator_result)
+      expect(subject.valid?(rq)).to eq(validator_result)
+    end
+  end
+end
