628fed1d8193b5f35df039d18da48092bdee4fcf
Damian Jaszczurowski <damian.jaszczurowski@gmail.com>
2016-06-21 21:56:46 +0200

    Validate whether request base_url and path make valid uri

--- lib/chartmogul_client/rq_validator.rb
+++ lib/chartmogul_client/rq_validator.rb
@@ -14,18 +14,41 @@ module ChartmogulClient::RqValidator
   end
 
   def self.run_basic_validation(request)
+    errors = validate_uri(request)
+    return [false, errors] unless errors.empty?
+
+    errors = validate_specific_api_version(request)
+    [errors.empty?, errors]
+  end
+
+  def self.validate_uri(request)
+    errors = []
+
+    if request.base_url
+      uri = URI.parse("#{request.base_url}#{request.path}")
+      unless (uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS))
+        errors << "invalid url: #{uri.to_s}"
+      end
+    else
+      errors << "missing base_url"
+    end
+
+    errors
+  end
+
+  def self.validate_specific_api_version(request)
+    errors = []
+
     case request.api_version
     when ChartmogulClient::Consts::ApiVersions::V1
-      errors = []
-
       if request.security_key.nil? || request.account_token.nil?
         errors << 'security_key/account_token missing'
       end
-
-      [errors.empty?, errors]
     else
       raise "Unknown API version #{request.api_version}"
     end
+
+    errors
   end
 
   def self.try_find_custom_validator(request)
--- spec/rq_validator_spec.rb
+++ spec/rq_validator_spec.rb
@@ -14,22 +14,45 @@ describe ChartmogulClient::RqValidator do
     end
 
     class NoValidatorRq < ChartmogulClient::V1::BaseRq
+      def path
+        '/test'
+      end
     end
 
     class NoValidMethodRq < ChartmogulClient::V1::BaseRq
+      def path
+        '/test'
+      end
     end
 
     class WithValidatorRq < ChartmogulClient::V1::BaseRq
+      def path
+        '/test'
+      end
     end
   end
 
   subject { described_class }
+
+  context 'when API base_url missing/invalid' do
+    it 'returns 400' do
+      rq = ChartmogulClient::V1::Test::NoValidatorRq.new
+      rq.security_key = 'security_key'
+      rq.account_token = 'account_token'
+
+      expect(subject.valid?(rq)).to eq([false, ['missing base_url']])
+
+      rq.base_url = "foo"
+      expect(subject.valid?(rq)).to eq([false, ['invalid url: foo/test']])
+    end
+  end
 
   context 'when no validator for request found' do
     it 'returns true' do
       rq = ChartmogulClient::V1::Test::NoValidatorRq.new
       rq.security_key = 'security_key'
       rq.account_token = 'account_token'
+      rq.base_url = "http://example.com"
 
       expect(subject.valid?(rq)).to eq([true, []])
     end
@@ -40,6 +63,7 @@ describe ChartmogulClient::RqValidator do
       rq = ChartmogulClient::V1::Test::NoValidMethodRq.new
       rq.security_key = 'security_key'
       rq.account_token = 'account_token'
+      rq.base_url = "http://example.com"
 
       expect { subject.valid?(rq) }.to raise_error("Validator ChartmogulClient::V1::Test::Validators::NoValidMethodRqValidator should respond to valid?")
     end
@@ -48,6 +72,7 @@ describe ChartmogulClient::RqValidator do
   context 'when API V1 request but security_key/account_token is missing' do
     it 'returns 400' do
       rq = ChartmogulClient::V1::Test::WithValidatorRq.new
+      rq.base_url = "http://example.com"
 
       expect(subject.valid?(rq)).to eq([false, ['security_key/account_token missing']])
     end
@@ -58,6 +83,7 @@ describe ChartmogulClient::RqValidator do
       rq = ChartmogulClient::V1::Test::WithValidatorRq.new
       rq.security_key = 'security_key'
       rq.account_token = 'account_token'
+      rq.base_url = "http://example.com"
 
       validator_result = [true, []]
 
